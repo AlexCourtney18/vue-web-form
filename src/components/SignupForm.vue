03ace79fb993aa362990243369a62b01708db919
Alex <lex.courtney18@gmail.com>
2023-02-19 13:55:45 -0600

    add form handler with password validation

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email" />
 
     <label>Password:</label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -21,6 +22,10 @@
     <div class="terms">
       <input type="checkbox" v-model="terms" required />
       <label>Accept Terms and Conditions</label>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
     </div>
   </form>
 
@@ -40,6 +45,7 @@ export default {
       terms: false,
       tempSkill: "",
       skills: [],
+      passwordError: '',
     };
   },
   methods: {
@@ -52,10 +58,23 @@ export default {
       }
     },
     deleteSkill(skill) {
-        this.skills = this.skills.filter((item) => {
-            return skill !== item
-        });
-    }
+      this.skills = this.skills.filter((item) => {
+        return skill !== item;
+      });
+    },
+    handleSubmit() {
+      // validate password
+        this.passwordError = this.password.length > 5 ? 
+        '' : 'Password must be at least 6 chars long'
+
+        if(!this.passwordError) {
+            console.log('email: ', this.email)
+            console.log('password: ', this.password)
+            console.log('role: ', this.role)
+            console.log('skills: ', this.skills)
+            console.log('terms accepted: ', this.terms)
+        }
+    },
   },
 };
 </script>
@@ -96,15 +115,33 @@ input[type="checkbox"] {
   top: 2px;
 }
 .pill {
- display: inline-block;
- margin: 20px 10px 0 0;
- padding: 6px 12px;
- background: #eee;
- border-radius: 20px;
- font-size: 12px;
- letter-spacing: 1px;
- font-weight: bold;
- color: #777;
- cursor: pointer;
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+  cursor: pointer;
+}
+.submit {
+  text-align: center;
+}
+.error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 </style>
